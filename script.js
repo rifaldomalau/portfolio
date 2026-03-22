5876dead86aebf7f069dab72424e7d71a8cfdd72
hotmalau <hotrifaldomalau@gmail.com>
2026-03-23 01:06:03 +0700

    push halaman techstack

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ document.addEventListener("mousemove", function (e) {
   bgText.style.transform = `translate(${x * 0.5}px, ${y * 0.5}px)`;
   heroImage.style.transform = `translate(${-x}px, ${-y}px)`;
 });
+
+// ================= SENSOR SCROLL NAVBAR =================
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+
+  // Jika layar digeser lebih dari 50 pixel ke bawah, aktifkan wujud kedua (blur)
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    // Jika kembali ke puncak (Home), matikan blurnya
+    header.classList.remove("scrolled");
+  }
+});
+
+// MANTRA PENYUNTIKAN FILE HTML EKSTERNAL
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("techstack.html")
+    .then((response) => {
+      if (!response.ok) throw new Error("File senjata gagal dimuat.");
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("techstack-container").innerHTML = data;
+    })
+    .catch((error) => console.error("Peringatan Matriks:", error));
+});
